3ead16287ce44fd0fdac30a1172ab55324686419
Lejla Musanovic <lejla_m96@live.com>
2023-04-30 17:26:32 +0200

    first homework for automation testing done

---
+++ src/test/java/com/placelab/utils/WebDriverSetup.java
@@ -0,0 +1,30 @@
+package com.placelab.utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class WebDriverSetup {
+
+    public static WebDriver getWebDriver(final String browserName) {
+        if (browserName.equals("edge")) {
+            return getEdgeDriver();
+        } else if (browserName.equals("chrome")) {
+            return getChromeDriver();
+        } else {
+            throw new IllegalArgumentException("The browser " + browserName + " is not found.");
+        }
+    }
+
+    private static WebDriver getEdgeDriver() {
+        WebDriverManager.edgedriver().setup();
+        return new EdgeDriver();
+    }
+
+    private static WebDriver getChromeDriver() {
+        WebDriverManager.chromedriver().browserVersion("112.0.5615.138").setup();
+
+        return new ChromeDriver();
+    }
+}
